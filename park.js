4c589f3ee5bd60dc17ec4e0c313842c50ddb5795
Stuart O'Donnell <stuartodonnell@gmail.com>
2018-06-06 10:32:30 +0100

    adds psuedocode comment to park_spec.js

--- park.js
+++ park.js
@@ -9,7 +9,7 @@ Park.prototype.addDinoToEnclosure = function(dinosaur){
 }
 
 Park.prototype.removeDinoByName = function(type){
-  for (i = 0; i < this.enclosure.length; i++){
+  for (let i = 0; i < this.enclosure.length; i++){
       if(this.enclosure[i].type === type) {
           this.enclosure.splice(i, 1);
           i--;
